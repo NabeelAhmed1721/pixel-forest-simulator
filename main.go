d3faa0546433f882e79bffe3197609f9a2f2719c
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-10-21 21:36:46 -0400

    camera

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"image"
 	_ "image/png"
+	"math"
 	"math/rand"
 	"os"
+	"time"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
@@ -50,17 +52,46 @@ func run() {
 	}
 
 	var (
-		trees    []*pixel.Sprite
-		matrices []pixel.Matrix
+		camPos       = pixel.ZV
+		camSpeed     = 5000.0
+		camZoom      = 1.0
+		camZoomSpeed = 1.2
+		trees        []*pixel.Sprite
+		matrices     []pixel.Matrix
 	)
 
+	last := time.Now()
 	for !win.Closed() {
+		dt := time.Since(last).Seconds()
+		last = time.Now()
+
+		if win.Pressed(pixelgl.KeyA) {
+			camPos.X -= camSpeed * dt
+		}
+		if win.Pressed(pixelgl.KeyD) {
+			camPos.X += camSpeed * dt
+		}
+		if win.Pressed(pixelgl.KeyS) {
+			camPos.Y -= camSpeed * dt
+		}
+		if win.Pressed(pixelgl.KeyW) {
+			camPos.Y += camSpeed * dt
+		}
+
+		camZoom *= math.Pow(camZoomSpeed, win.MouseScroll().Y)
+
 		win.Clear(colornames.Forestgreen)
+
+		cam := pixel.IM.Scaled(camPos, camZoom).Moved(win.Bounds().Center().Sub(camPos))
+		win.SetMatrix(cam)
+
 		if win.JustPressed(pixelgl.MouseButtonLeft) {
 			// Random tree
 			tree := pixel.NewSprite(treeSpritesheet, treeFrames[rand.Intn(len(treeFrames))])
 			trees = append(trees, tree) // append tree sprite to array
-			matrices = append(matrices, pixel.IM.Scaled(pixel.ZV, 4).Moved(win.MousePosition()))
+			mouse := cam.Unproject(win.MousePosition())
+
+			matrices = append(matrices, pixel.IM.Scaled(pixel.ZV, 16).Moved(mouse))
 		}
 
 		if win.JustPressed(pixelgl.MouseButtonRight) {
